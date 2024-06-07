b990eeb78c48a8f73ceceb9412e9ec352a294971
benj-lazaro <benjie.work@gmail.com>
2024-06-07 19:42:41 +0800

    Section 05 template literals initial commit

---
+++ 01-fundamentals/05-objects/13-template-literals/code/app.js
@@ -0,0 +1,16 @@
+// Template Literals Demo
+
+const name = "John";
+const continentalOwner = "Winston";
+
+const message = `
+  Hi ${name},
+
+  Thank you for joining the Agent ${45 + 2} Program mailing list.
+
+
+  Regards,
+  ${continentalOwner}
+`;
+
+console.log(message);
