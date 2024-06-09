a19b9ac538376127b5ce3d26bfad9c84939cd236
benj-lazaro <benjie.work@gmail.com>
2024-06-09 17:29:59 +0800

    Section 02 primitive types initial commit

---
+++ 01-fundamentals/02-basics/03-primitive-types/code/app.js
@@ -0,0 +1,17 @@
+// Primitive Types
+
+// String literal
+let userName = "Mosh";
+
+// Number literal
+let userAge = 30;
+
+// Boolean literal
+let isApproved = true;
+
+// undefined; default value of an uninitialized variable
+let undefinedVariable;
+let declaredUndefinedVariable = undefined;
+
+// null
+let nullVariable = null;
