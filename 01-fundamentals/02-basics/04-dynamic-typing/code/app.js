c068b1aaab7c5246c03f23ab68d0e4b0a345d2c2
benj-lazaro <benjie.work@gmail.com>
2024-06-09 18:38:04 +0800

    Section 02 dynamic typing initial commit

---
+++ 01-fundamentals/02-basics/04-dynamic-typing/code/app.js
@@ -0,0 +1,20 @@
+// Dynamic Typing
+
+// Declare a variable with a string literal
+let userName = "Mosh";
+console.log(typeof userName);
+
+// Dynamically change the value of the variable
+userName = 1;
+console.log(typeof userName);
+
+let isApproved = true;
+console.log(typeof isApproved);
+
+// Assign the value of "undefined" (which is also a type) to variable firstName
+let firstName = undefined;
+console.log(typeof firstName);
+
+// Returns an "object" type despite assigning a "null" value
+let selectedColor = null;
+console.log(typeof selectedColor);
