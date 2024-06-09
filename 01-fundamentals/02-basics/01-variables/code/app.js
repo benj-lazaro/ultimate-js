0dc16be9e3c8ebb90fb70de19136f19567b82742
benj-lazaro <benjie.work@gmail.com>
2024-06-09 16:35:33 +0800

    Section 02 variables initial commit

---
+++ 01-fundamentals/02-basics/01-variables/code/app.js
@@ -0,0 +1,16 @@
+// Variables
+
+// Variable declaration
+let userName = "Mosh";
+
+console.log(userName);
+
+// Declaring multiple variables on a single line
+let firstName, lastName, middleName;
+
+// Declaring each variables on a single line (Modern Best Practice)
+let buildingNumber;
+let unitNumber;
+let streetName;
+let cityName;
+let zipCode;
