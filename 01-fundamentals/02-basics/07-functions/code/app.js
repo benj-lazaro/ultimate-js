a26760d4372fef45e55df6c7626ea5a80d73448a
benj-lazaro <benjie.work@gmail.com>
2024-06-10 23:19:28 +0800

    Section 02 functions initial commit

---
+++ 01-fundamentals/02-basics/07-functions/code/app.js
@@ -0,0 +1,9 @@
+// Functions
+
+// Declaring a function with the parameter variables
+function greet(firstName, lastName) {
+  console.log("Hello " + firstName + " " + lastName + "!");
+}
+
+// Calling the function with an argument value
+greet("John", "Wick");
