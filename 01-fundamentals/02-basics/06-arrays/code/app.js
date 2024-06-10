5f2ce3bf69949b79e1e10747ef48a26447ea3396
benj-lazaro <benjie.work@gmail.com>
2024-06-10 21:43:47 +0800

    Section 02 arrays initial commit

---
+++ 01-fundamentals/02-basics/06-arrays/code/app.js
@@ -0,0 +1,24 @@
+// Arrays
+
+// Declare & initialize an array with two elements
+const selectedColors = ["red", "blue"];
+
+// Display the content of the array
+console.log(selectedColors);
+
+// Display a specific array element using its index
+console.log(selectedColors[0]);
+
+// Add a new element on the 3rd index of the array
+selectedColors[2] = "green";
+console.log(selectedColors);
+
+// Add a new element that is a Number literal
+selectedColors[3] = 1;
+console.log(selectedColors);
+
+// Check the type of the array
+console.log(typeof selectedColors);
+
+// Access the .length property of the array
+console.log(selectedColors.length);
