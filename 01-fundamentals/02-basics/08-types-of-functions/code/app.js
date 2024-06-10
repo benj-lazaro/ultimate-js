d9b24c083c0cda481c44db550a9e6e907b59d97e
benj-lazaro <benjie.work@gmail.com>
2024-06-11 00:21:51 +0800

    Section 02 types of functions initial commit

---
+++ 01-fundamentals/02-basics/08-types-of-functions/code/app.js
@@ -0,0 +1,14 @@
+// Types of Functions
+
+// Performs a task = display a message
+function greet(firstName, lastName) {
+  console.log("Hello " + firstName + " " + lastName + "!");
+}
+
+// Calculates a value = value of a squared number
+function square(number) {
+  return number * number;
+}
+
+// Calls the function
+console.log(square(2));
