9fae91dced7dca5f922e0b98ebf7d6d909394ce9
benj-lazaro <benjie.work@gmail.com>
2024-06-11 16:50:19 +0800

    Section 03 equality operators initial commit

---
+++ 01-fundamentals/03-operators/05-equality-operators/code/app.js
@@ -0,0 +1,11 @@
+// Equality Operators
+
+// Strict Equality
+console.log(1 === 1);
+console.log("1" === 1); // Returns false due to the difference of type
+console.log(true === 1);
+
+// Lose Equality
+console.log(1 == 1);
+console.log("1" == 1); // Returns true due to the equality of value alone
+console.log(true == 1); // Converts the right side into a boolean w/ 1 being equal to true
