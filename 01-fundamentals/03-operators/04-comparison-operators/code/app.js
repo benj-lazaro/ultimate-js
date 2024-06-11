f0f6edc6c0bea08d24cb9a08a9f17d81523f7f8f
benj-lazaro <benjie.work@gmail.com>
2024-06-11 15:13:58 +0800

    Section 03 comparison operators initial commit

---
+++ 01-fundamentals/03-operators/04-comparison-operators/code/app.js
@@ -0,0 +1,25 @@
+// Comparison Operators
+
+let x = 1;
+
+// Relational Operators
+// Greater than
+console.log(x > 0);
+
+// Greater than or equal to
+console.log(x >= 1);
+
+// Less than
+console.log(x < 1);
+
+// Less than or equal to
+console.log(x <= 1);
+console.log("----------------");
+
+// Equality Operators
+// Equal to
+console.log(x === 1);
+
+// Not equal to
+console.log(x !== 1);
+console.log("----------------");
