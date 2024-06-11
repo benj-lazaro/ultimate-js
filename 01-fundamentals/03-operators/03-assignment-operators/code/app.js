4dda60dd7b13565e50e4bf73f5ad6ecafc194854
benj-lazaro <benjie.work@gmail.com>
2024-06-11 14:47:29 +0800

    Section 03 assignment operator initial commit

---
+++ 01-fundamentals/03-operators/03-assignment-operators/code/app.js
@@ -0,0 +1,19 @@
+// Assignment Operators
+
+let x = 10;
+console.log(x);
+
+x = x + 1; // Equivalent to x++
+console.log(x);
+x = x - 1; // Equivalent to x--
+console.log(x);
+
+// Shorthand assignment operator
+x += 1;
+console.log(x);
+
+x -= 1;
+console.log(x);
+
+x *= 3; // Equivalent to x = x * 3
+console.log(x);
