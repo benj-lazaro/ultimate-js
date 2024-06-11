592f6f34f834887372ce02df29b8d78cb9aab459
benj-lazaro <benjie.work@gmail.com>
2024-06-11 14:35:32 +0800

    Section 03 arithmetic operators initial commit

---
+++ 01-fundamentals/03-operators/02-arithmetic-operators/code/app.js
@@ -0,0 +1,41 @@
+// Arithmetic Operators
+
+let x = 10;
+let y = 3;
+
+// Addition
+console.log(x + y);
+
+// Subtraction
+console.log(x - y);
+
+// Multiplication
+console.log(x * y);
+
+// Division
+console.log(x / y);
+
+// Remainder / Modulus
+console.log(x % y);
+
+// Exponentiation
+console.log(x ** y);
+
+console.log("--------------");
+
+// Increment
+console.log(x);
+console.log(x++); // Increments the value of x after calling the console.log()
+console.log(x); // Value is now incremented by 1
+console.log("--------------");
+x = 10; // Resets variable's value to its initial state
+console.log(++x); // The value of x is incremented before calling console.log()
+console.log("--------------");
+
+// Decrement
+console.log(y);
+console.log(y--); // Decrements the value of y after calling the console.log()
+console.log(y); // Value is not decremented by 1
+console.log("--------------");
+y = 3; // Resets variable's value to its initial state
+console.log(--y); // The value of y is decremented before calling console.log()
