fca6d4a680c1d58f8daca4955e9d4ea27c0fb7a3
benj-lazaro <benjie.work@gmail.com>
2024-06-11 17:13:25 +0800

    Section 03 ternary operator initial commit

---
+++ 01-fundamentals/03-operators/06-ternary-operator/code/app.js
@@ -0,0 +1,12 @@
+// Ternary Operator
+
+// If a customer has more than 100 points,
+// they are a "gold" customer otherwise,
+// they are a "silver" customer
+
+let points = 110;
+
+// Ternary Operator
+let type = points > 100 ? "gold" : "silver";
+
+console.log("The customer is " + type);
