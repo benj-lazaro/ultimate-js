637fa3a20382ff68e79f3638a7ec00c4db128cde
benj-lazaro <benjie.work@gmail.com>
2024-06-12 11:05:42 +0800

    Section 04 if-else intial commit

---
+++ 01-fundamentals/04-control-flow/01-if-else/code/app.js
@@ -0,0 +1,12 @@
+// If..else
+
+// Hour is based on a 24-hour time format
+let currentHour = 10;
+
+if (currentHour >= 6 && currentHour < 12) {
+  console.log("Good morning");
+} else if (currentHour >= 12 && currentHour < 18) {
+  console.log("Good afternoon");
+} else {
+  console.log("Good evening");
+}
