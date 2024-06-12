35f22dbbb42abe530262d29d7c008226df1b9a44
benj-lazaro <benjie.work@gmail.com>
2024-06-12 11:51:12 +0800

    Section 04 for initial commit

---
+++ 01-fundamentals/04-control-flow/03-for/code/app.js
@@ -0,0 +1,20 @@
+// For
+
+// Display "Hello World" five times in the console
+for (let index = 0; index < 5; index++) {
+  console.log("Hello World! ", index);
+}
+
+console.log("---------");
+
+// Display odd number between 1 to 5
+for (let i = 1; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
+
+console.log("---------");
+
+// Display odd number between 1 to 5 in reverse order
+for (let i = 5; i >= 1; i--) {
+  if (i % 2 !== 0) console.log(i);
+}
