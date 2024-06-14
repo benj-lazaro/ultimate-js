acc956e0ae817e3008289ad60c9a08df179041a8
benj-lazaro <benjie.work@gmail.com>
2024-06-14 13:27:08 +0800

    Section 06 removing elements initial commit

---
+++ 01-fundamentals/06-arrays/06-removing-elements/code/app.js
@@ -0,0 +1,19 @@
+// Arrow Functions Demo
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+
+// Remove an element from the end
+const lastElement = numbers.pop();
+console.log("Last element: ", lastElement);
+console.log(numbers);
+
+// Remove an element from the beginning
+const firstElement = numbers.shift();
+console.log("First element: ", firstElement);
+console.log(numbers);
+
+// Remove element(s) from the middle or specific location
+const removedElements = numbers.splice(2, 3);
+console.log("Removed elements: ", removedElements);
+console.log(numbers);
