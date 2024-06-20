2ec9137d1ee380a8783f1b3b2ecf7ae60476bfc2
benj-lazaro <benjie.work@gmail.com>
2024-06-20 16:55:35 +0800

    Section 02 factories initial commit

---
+++ 02-advanced/02-objects/02-factories/code/app.js
@@ -0,0 +1,23 @@
+// Factories Demo
+
+// Create an object using factory or constructor function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("Drawing the circle...");
+    },
+  };
+}
+
+// Create an object with a radius of 10
+const circle1 = createCircle(10);
+console.log(circle1);
+
+// Create another circle object with a radius of 50
+const circle2 = createCircle(50);
+console.log(circle2);
+
+// Calling the method draw()
+circle1.draw();
+circle2.draw();
